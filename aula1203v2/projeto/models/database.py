c2bcccc8904e6bbae9a8b68b2523cd1b5ab58a8d
Euler <eulerborgesf@gmail.com>
2025-03-26 09:11:09 -0300

    aula do dia 26/03

--- aula1203v2/projeto/models/database.py
+++ aula1203v2/projeto/models/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:banco123@localhost:5432/universidade"
+DATABASE_URL = os.getenv("DATABASE_URL")
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:banco123@localhost:5432/universidade"
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
